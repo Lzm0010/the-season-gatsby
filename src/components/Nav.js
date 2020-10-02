f350f3f9bcd84563b99f8f265f22d7d55996d2ee
lzm0010 <lzm0010@gmail.com>
2020-10-02 11:52:23 -0500

    styled nav and layout

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,67 @@
 import React from 'react';
 import {Link} from 'gatsby';
+import styled from 'styled-components';
+import Logo from './Logo';
+
+
+const NavStyles = styled.nav`
+    margin-bottom: 3rem;
+    .logo {
+        transform: translateY(-25%);
+    }
+    ul {
+        margin: 0;
+        padding: 0;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr auto 1fr 1fr 1fr 1fr;
+        grid-gap: 2rem;
+        align-items: center;
+        text-align: center;
+        list-style: none;
+        margin-top: -6rem;
+    }
+    li {
+        --rotate: -2deg;
+        transform: rotate(var(--rotate));
+        order: 1;
+        &:nth-child(1) {
+            --rotate: 1deg;
+        }
+        &:nth-child(2) {
+            --rotate: -2.5deg;
+        }
+        &:nth-child(3) {
+            --rotate: -2.5deg;
+        }
+        &:nth-child(4) {
+            --rotate: -2.5deg;
+        }
+        &:nth-child(6) {
+            --rotate: 5deg;
+        }
+        &:nth-child(7) {
+            --rotate: 5deg;
+        }
+        &:nth-child(8) {
+            --rotate: 5deg;
+        }
+        &:hover {
+            --rotate: 3deg;
+        }
+    }
+    a {
+        font-size: 1.8rem;
+        text-decoration: none;
+        &:hover {
+            color: var(--red);
+        }
+    }
+`;
 
 export default function Nav() {
     return (
-        <nav>
+        <NavStyles>
             <ul>
-                <li>
-                    <Link to={'/'}>
-                        Home
-                    </Link>
-                </li>
                 <li>
                     <Link to={'/game'}>
                         Play Game
@@ -31,8 +83,8 @@ export default function Nav() {
                     </Link>
                 </li>
                 <li>
-                    <Link to={'/news'}>
-                        News
+                    <Link to={'/'}>
+                        <Logo />
                     </Link>
                 </li>
                 <li>
@@ -55,12 +107,7 @@ export default function Nav() {
                         Stats
                     </Link>
                 </li>
-                <li>
-                    <Link to={'/history'}>
-                        History
-                    </Link>
-                </li>
             </ul>
-        </nav>
+        </NavStyles>
     );
 }
